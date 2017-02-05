67879d088bfc7ce49eb57b62a61bdc54b692ff14
Seve Sheldon <sevesheldon@gmail.com>
2017-02-04 18:58:16 -0800

    Add the fourth and final spec to our business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-//Business interface logic
+//Business logic
 
 var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
